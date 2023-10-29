7887f6a0669409ba03e081b7f948d5c0bef69cff
saied nasiripour <saied.nasiripour@gmail.com>
2023-10-29 20:35:46 +0330

    modify the name of the WeatherRepositoryInterface

--- lib/features/weather_feature/domain/repositories/weather_repository_abstract.dart
+++ lib/features/weather_feature/domain/repositories/weather_repository_abstract.dart
@@ -2,7 +2,7 @@
 import 'package:weather_1_flutter/core/resources/data_state.dart';
 import 'package:weather_1_flutter/features/weather_feature/domain/entities/current_city_entity.dart';
 
-abstract class WeatherRepositoryInterface {
+abstract class WeatherRepositoryAbstract {
 
   Future<DataState<CurrentCityEntity>> fetchCurrentWeatherData(String cityName);
 }
