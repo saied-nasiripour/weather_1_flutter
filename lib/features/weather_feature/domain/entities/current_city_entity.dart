e21deb21fabcfc18998012823a8d861f58a54bf5
saied nasiripour <saied.nasiripour@gmail.com>
2023-10-29 15:43:19 +0330

    generate CurrentCityModel and CurrentCityEntity from JSON data

---
+++ lib/features/weather_feature/domain/entities/current_city_entity.dart
@@ -0,0 +1,47 @@
+/*
+ Entity == Data Holder
+ This class's only Task is to act as an information keeper (or use Variables)
+ data => domain(Entity) => presentation
+*/
+
+import 'package:equatable/equatable.dart';
+import 'package:weather_1_flutter/features/weather_feature/data/models/current_city_model.dart';
+
+class CurrentCityEntity extends Equatable {
+
+  const CurrentCityEntity({
+    this.coord,
+    this.weather,
+    this.base,
+    this.main,
+    this.visibility,
+    this.wind,
+    this.rain,
+    this.clouds,
+    this.dt,
+    this.sys,
+    this.timezone,
+    this.id,
+    this.name,
+    this.cod,
+});
+
+  final Coord? coord;
+  final List<Weather>? weather;
+  final String? base;
+  final Main? main;
+  final num? visibility;
+  final Wind? wind;
+  final Rain? rain;
+  final Clouds? clouds;
+  final num? dt;
+  final Sys? sys;
+  final num? timezone;
+  final num? id;
+  final String? name;
+  final num? cod;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
