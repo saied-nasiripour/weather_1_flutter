0a050260660d0c4e3845666442d125ec2a980681
saied nasiripour <saied.nasiripour@gmail.com>
2023-10-29 20:44:19 +0330

    add comment to the WeatherRepositoryInterface

--- lib/features/weather_feature/domain/repositories/weather_repository_abstract.dart
+++ lib/features/weather_feature/domain/repositories/weather_repository_abstract.dart
@@ -2,6 +2,7 @@
 import 'package:weather_1_flutter/core/resources/data_state.dart';
 import 'package:weather_1_flutter/features/weather_feature/domain/entities/current_city_entity.dart';
 
+/// interface ( or abstract) Module for the Implementation Module within the data layer
 abstract class WeatherRepositoryAbstract {
 
   Future<DataState<CurrentCityEntity>> fetchCurrentWeatherData(String cityName);
