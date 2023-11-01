f2c094bee081a9bfba21ad09cc59d7214553fd8e
saied nasiripour <saied.nasiripour@gmail.com>
2023-11-01 16:53:46 +0330

    generate the HomeBloc

--- lib/features/weather_feature/presentation/bloc/home_bloc.dart
+++ lib/features/weather_feature/presentation/bloc/home_bloc.dart
@@ -1,16 +1,29 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:weather_1_flutter/core/resources/data_state.dart';
+import 'package:weather_1_flutter/features/weather_feature/domain/use_cases/get_current_weather_usecase.dart';
 import 'package:weather_1_flutter/features/weather_feature/presentation/bloc/cw_status.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeInitial()) {
-    on<LoadCwEvent>((event, emit) {
-      // TODO: implement event handler
+  final GetCurrentWeatherUseCase getCurrentWeatherUseCase;
+  HomeBloc(this.getCurrentWeatherUseCase) : super(HomeState(cwStatus: CwLoading())) {
+    /// Type of state: HomeState()
+
+    on<LoadCwEvent>((event, emit) async{
+      emit(state.copyWith(newCwStatus: CwLoading()));
+
+      DataState dataState = await getCurrentWeatherUseCase(event.cityName);
+
+      if (dataState is DataSuccess) {
+        emit(state.copyWith(newCwStatus: CwCompleted(dataState.data)));
+      } else if (dataState is DataFailed) {
+        emit(state.copyWith(newCwStatus: CwError(dataState.error!)));
+      }
+
     });
+
   }
 }
