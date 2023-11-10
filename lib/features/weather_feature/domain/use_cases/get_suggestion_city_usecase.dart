1c3eb69710eab3bb8e1c2ac0a652323dd5e9f9ad
saied nasiripour <saied.nasiripour@gmail.com>
2023-11-10 15:19:03 +0330

    modify the GetSuggestionCityUseCase

--- lib/features/weather_feature/domain/use_cases/get_suggestion_city_usecase.dart
+++ lib/features/weather_feature/domain/use_cases/get_suggestion_city_usecase.dart
@@ -5,12 +5,12 @@ import 'package:weather_1_flutter/features/weather_feature/data/models/suggest_c
 import 'package:weather_1_flutter/features/weather_feature/domain/repositories_abstract/weather_repository_abstract.dart';
 
 class GetSuggestionCityUseCase implements UseCase<List<Data>, String>{
-  final WeatherRepositoryAbstract _weatherRepository;
-  GetSuggestionCityUseCase(this._weatherRepository);
+  final WeatherRepositoryAbstract _weatherRepositoryAbstract;
+  GetSuggestionCityUseCase(this._weatherRepositoryAbstract);
 
   @override
   Future<List<Data>> call(String params) {
-    return _weatherRepository.fetchSuggestData(params);
+    return _weatherRepositoryAbstract.fetchSuggestData(params);
   }
 
 }
