90f9bfa3550f366482a6e991c6c9d32a57dfd11c
saied nasiripour <saied.nasiripour@gmail.com>
2023-11-16 06:11:36 +0330

    add some comments

--- lib/features/bookmark_feature/data/data_source/local/city_dao.dart
+++ lib/features/bookmark_feature/data/data_source/local/city_dao.dart
@@ -4,15 +4,18 @@ import 'package:weather_1_flutter/features/bookmark_feature/domain/entities/city
 @dao
 abstract class CityDao {
 
+  // table name == entity name ==> CityEntity
   @Query('SELECT * FROM CityEntity')
   Future<List<CityEntity>> getAllCity();
 
+  // table name == entity name ==> CityEntity
   @Query('SELECT * FROM CityEntity WHERE name = :name')
   Future<CityEntity?> findCityByName(String name);
 
   @insert
   Future<void> insertCity(CityEntity city);
 
+  // table name == entity name ==> CityEntity
   @Query('SELECT FROM CityEntity WHERE name = :name')
   Future<void> deleteCityByName(String name);
 }
